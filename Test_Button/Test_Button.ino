abf4ed5c57185dbe1cdb660a17383cc921030b1b
Kim Dao <163894501+KimmyDao@users.noreply.github.com>
2024-10-31 13:44:03 -0400

    Update Test_Button.ino
    Updating the code

--- Test_Button/Test_Button.ino
+++ Test_Button/Test_Button.ino
@@ -5,6 +5,12 @@
 
 Adafruit_NeoPixel pixels (NUMPIXELS, 9, NEO_GRB + NEO_KHZ800);
 
+#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+
+Adafruit_7segment matrix = Adafruit_7segment();
+
 int x;
 int y;
 
@@ -19,18 +25,35 @@ int y;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  // pinMode(5, INPUT);
+#ifndef __AVR_ATtiny85__
+  Serial.begin(9600);
+  Serial.println("7 Segment Backpack Test");
+#endif
+  matrix.begin(0x70);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   pixels.begin();
-  x = digitalRead(5);
-  //Serial.println(x);
+  x = analogRead(3);
+  y = analogRead(2);
+  //  Serial.println(x);
   delay(200);
+
+  // Displaying value of Pentiometer 1
+  matrix.print(x);
+  matrix.writeDisplay();
+  delay(50);
+
+  // Displaying value of Pentiometer 2
+  matrix.print(y);
+  matrix.writeDisplay();
+  delay(50);
 
   if ((x == Green) && (y == Green)) {
     Serial.println("GREEN");
+    Serial.println(x);
+    Serial.println(y);
     for (int i = 0; i < NUMPIXELS; i++) {
       pixels.setPixelColor(i, pixels.Color(0, 150, 0));
     }
@@ -38,6 +61,8 @@ void loop() {
 
   if ((x == Red) && (y == Red)) {
     Serial.println("RED");
+    Serial.println(x);
+    Serial.println(y);
     for (int i = 0; i < NUMPIXELS; i++) {
       pixels.setPixelColor(i, pixels.Color(150, 0, 0));
     }
@@ -45,6 +70,8 @@ void loop() {
 
   if ((x > Blue) && (y < Red)) {
     Serial.println("PURPLE");
+    Serial.println(x);
+    Serial.println(y);
     for (int i = 0; i < NUMPIXELS; i++) {
       pixels.setPixelColor(i, pixels.Color(50, 0, 150));
     }
@@ -52,6 +79,8 @@ void loop() {
 
   if ((x / 2 < Red) && (y < Green)) {
     Serial.println("YELLOW");
+    Serial.println(x);
+    Serial.println(y);
     for (int i = 0; i < NUMPIXELS; i++) {
       pixels.setPixelColor(i, pixels.Color(100, 150, 0));
     }
